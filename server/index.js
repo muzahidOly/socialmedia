77d9b217bb404f2d94ec8304e8e145aa95636b4f
Muzahid Oly <93054530+muzahidOly@users.noreply.github.com>
2022-07-06 14:51:37 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 
 app.use('/posts',postRoutes);
-const CONNECTION_URL='mongodb+srv://social:network@cluster0.jgrkfkv.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL='mongodb+srv://social:<PASSWORD>@cluster0.jgrkfkv.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT=process.env.PORT || 5000;
 
